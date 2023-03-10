688bbcbe79649ae19ebbd2389e73dba6aef07558
Souvik Patra (Patra) <souvik.patra@souvik.patra-TP61LXN224>
2023-03-10 14:47:48 +0530

    completed authentication

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -1,25 +1,58 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core'
-
 import * as React from 'react'
-// 🐨 you're going to need this:
-// import * as auth from 'auth-provider'
+import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {client} from 'utils/api-client'
+import {useAsync} from 'utils/hooks'
+import {FullPageSpinner} from 'components/lib'
+import * as colors from './styles/colors.js'
+
+const getUser = async () => {
+  const token = await auth.getToken()
+  let user = null
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+  return user
+}
 
 function App() {
-  // 🐨 useState for the user
+  const {data, error, isIdle, isLoading, isSuccess, isError, run, setData} =
+    useAsync({status: 'idle', data: null, error: null})
+  const login = form => auth.login(form).then(u => setData(u))
+  const register = form => auth.register(form).then(u => setData(u))
+  const logout = () => {
+    auth.logout()
+    setData(null)
+  }
 
-  // 🐨 create a login function that calls auth.login then sets the user
-  // 💰 const login = form => auth.login(form).then(u => setUser(u))
-  // 🐨 create a registration function that does the same as login except for register
-
-  // 🐨 create a logout function that calls auth.logout() and sets the user to null
-
-  // 🐨 if there's a user, then render the AuthenticatedApp with the user and logout
-  // 🐨 if there's not a user, then render the UnauthenticatedApp with login and register
-
-  return <UnauthenticatedApp />
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
+  return isError ? (
+    <div
+      css={{
+        color: colors.danger,
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <p>Uh oh... There's a problem. Try refreshing the app.</p>
+      <pre>{error.message}</pre>
+    </div>
+  ) : isIdle || isLoading ? (
+    <FullPageSpinner />
+  ) : data === null ? (
+    <UnauthenticatedApp login={login} register={register} />
+  ) : (
+    <AuthenticatedApp user={data} logout={logout} />
+  )
 }
 
 export {App}
