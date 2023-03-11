ca911e9b2aed27eecd6b3d18edec93ac71cf04a2
Souvik Patra (Patra) <souvik.patra@souvik.patra-TP61LXN224>
2023-03-11 19:57:10 +0530

    completed unit testing part 1

--- src/utils/__tests__/api-client.exercise.js
+++ src/utils/__tests__/api-client.exercise.js
@@ -1,56 +1,88 @@
 // 🐨 you'll need the test server
 // 💰 the way that our tests are set up, you'll find this in `src/test/server/test-server.js`
-// import {server, rest} from 'test/server'
+import {server, rest} from 'test/server'
+import {client} from 'utils/api-client'
+const apiURL = process.env.REACT_APP_API_URL
 // 🐨 grab the client
 // import {client} from '../api-client'
 
-// 🐨 add a beforeAll to start the server with `server.listen()`
-// 🐨 add an afterAll to stop the server when `server.close()`
-// 🐨 afterEach test, reset the server handlers to their original handlers
-// via `server.resetHandlers()`
+beforeAll(() => {
+  server.listen()
+})
+afterAll(() => {
+  server.close()
+})
+
+afterEach(() => {
+  server.resetHandlers()
+})
 
 // 🐨 flesh these out:
 
-test.todo('calls fetch at the endpoint with the arguments for GET requests')
-// 🐨 add a server handler to handle a test request you'll be making
-// 💰 because this is the first one, I'll give you the code for how to do that.
-// const endpoint = 'test-endpoint'
-// const mockResult = {mockValue: 'VALUE'}
-// server.use(
-//   rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
-//     return res(ctx.json(mockResult))
-//   }),
-// )
-//
-// 🐨 call the client (don't forget that it's asynchronous)
-// 🐨 assert that the resolved value from the client call is correct
+test('calls fetch at the endpoint with the arguments for GET requests', async () => {
+  const endpoint = 'test-endpoint'
+  const mockResult = {mockValue: 'VALUE'}
+  server.use(
+    rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      return res(ctx.json(mockResult))
+    }),
+  )
+  const data = await client(endpoint)
+  expect(data).toEqual(mockResult)
+})
 
-test.todo('adds auth token when a token is provided')
-// 🐨 create a fake token (it can be set to any string you want)
-// 🐨 create a "request" variable with let
-// 🐨 create a server handler to handle a test request you'll be making
-// 🐨 inside the server handler, assign "request" to "req" so we can use that
-//     to assert things later.
-//     💰 so, something like...
-//       async (req, res, ctx) => {
-//         request = req
-//         ... etc...
-//
-// 🐨 call the client with the token (note that it's async)
-// 🐨 verify that `request.headers.get('Authorization')` is correct (it should include the token)
+test('adds auth token when a token is provided', async () => {
+  // 🐨 create a fake token (it can be set to any string you want)
+  const endpoint = 'test-endpoint'
+  const fakeToken = 'myFakeToken'
+  const mockResult = {mockValue: 'VALUE'}
+  let request
 
-test.todo('allows for config overrides')
-// 🐨 do a very similar setup to the previous test
-// 🐨 create a custom config that specifies properties like "mode" of "cors" and a custom header
-// 🐨 call the client with the endpoint and the custom config
-// 🐨 verify the request had the correct properties
+  server.use(
+    rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      request = req
+      return res(ctx.json(mockResult))
+    }),
+  )
+  await client(endpoint, {token: fakeToken})
+  expect(request.headers.get('Authorization')).toBe(`Bearer ${fakeToken}`)
+})
+test('allows for config overrides', async () => {
+  const endpoint = 'test-endpoint'
+  const fakeToken = 'myFakeToken'
+  const customConfig = {
+    mode: 'cors',
+    headers: {
+      myHeader: 'MYHEADER',
+    },
+  }
+  const mockResult = {mockValue: 'VALUE'}
+  let request
 
-test.todo(
-  'when data is provided, it is stringified and the method defaults to POST',
-)
-// 🐨 create a mock data object
-// 🐨 create a server handler very similar to the previous ones to handle the post request
-//    💰 Use rest.post instead of rest.get like we've been doing so far
-// 🐨 call client with an endpoint and an object with the data
-//    💰 client(endpoint, {data})
-// 🐨 verify the request.body is equal to the mock data object you passed
+  server.use(
+    rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      request = req
+      return res(ctx.json(mockResult))
+    }),
+  )
+  await client(endpoint, {token: fakeToken, ...customConfig})
+  expect(request.headers.get('myHeader')).toBe('MYHEADER')
+  expect(request['mode']).toBe('cors')
+})
+
+test('when data is provided, it is stringified and the method defaults to POST', async () => {
+  const endpoint = 'test-endpoint'
+  const mockResult = {mockValue: 'VALUE'}
+  let request
+  const data = {
+    customData: 'MyCustomData',
+  }
+  server.use(
+    rest.post(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      request = req
+      return res(ctx.json(mockResult))
+    }),
+  )
+  await client(endpoint, {data: data})
+  expect(request.body).toEqual(data)
+})
--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,7 @@
-test.todo('formatDate formats the date to look nice')
+// test.todo('formatDate formats the date to look nice')
 
+const {formatDate} = require('utils/misc')
+
+test('formatDate formats the date to look', () => {
+  expect(formatDate(new Date('22 December 2008'))).toBe('Dec 08')
+})
