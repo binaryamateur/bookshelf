62e1d55511429538f8fa6a0214c607211d7e83f6
Souvik Patra (Patra) <souvik.patra@souvik.patra-TP61LXN224>
2023-03-12 11:06:03 +0530

    completed integration tests part 1

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,15 +1,81 @@
 // 🐨 here are the things you're going to need for this test:
-// import * as React from 'react'
-// import {render, screen, waitFor} from '@testing-library/react'
-// import {queryCache} from 'react-query'
-// import {buildUser, buildBook} from 'test/generate'
-// import * as auth from 'auth-provider'
-// import {AppProviders} from 'context'
-// import {App} from 'app'
+import * as React from 'react'
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
+import {queryCache} from 'react-query'
+import {buildUser, buildBook} from 'test/generate'
+import * as auth from 'auth-provider'
+import {AppProviders} from 'context'
+import {App} from 'app'
 
 // 🐨 after each test, clear the queryCache and auth.logout
+afterEach(async () => {
+  queryCache.clear()
+  await auth.logout()
+})
 
-test.todo('renders all the book information')
+test('renders all the book information', async () => {
+  window.localStorage.setItem(auth.localStorageKey, 'myTestKey')
+  const user = buildUser()
+  const book = buildBook()
+  window.history.pushState({}, 'fake book', `/book/${book.id}`)
+  const originalFetch = window.fetch
+  window.fetch = async (url, config) => {
+    console.warn(url, config)
+    if (url.includes('/bootstrap')) {
+      return Promise.resolve({
+        ok: true,
+        json: async () => {
+          return {user: {...user, token: 'myTestKey'}, listItems: []}
+        },
+      })
+    } else if (url.includes(`/list-items`)) {
+      return Promise.resolve({
+        ok: true,
+        json: async () => {
+          return {listItems: []}
+        },
+      })
+    } else if (url.includes(`/books/${book.id}`)) {
+      return Promise.resolve({
+        ok: true,
+        json: async () => {
+          return {
+            book,
+          }
+        },
+      })
+    }
+    return originalFetch(url, config)
+  }
+  render(<App />, {wrapper: AppProviders})
+  await waitForElementToBeRemoved(() => screen.queryAllByLabelText(/loading/i))
+  await waitForElementToBeRemoved(() => screen.queryAllByText(/loading/i))
+  expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
+  expect(screen.getByText(book.author)).toBeInTheDocument()
+  expect(screen.getByText(book.publisher)).toBeInTheDocument()
+  expect(screen.getByText(book.synopsis)).toBeInTheDocument()
+  expect(screen.getByRole('img', {name: /book cover/i})).toHaveAttribute(
+    'src',
+    book.coverImageUrl,
+  )
+  expect(screen.getByRole('button', {name: /add to list/i})).toBeInTheDocument()
+
+  expect(
+    screen.queryByRole('button', {name: /remove from list/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', {name: /mark as read/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', {name: /mark as unread/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('textbox', {name: /notes/i}),
+  ).not.toBeInTheDocument()
+  expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
+  expect(screen.queryByLabelText(/start date/i)).not.toBeInTheDocument()
+  screen.debug()
+})
 // 🐨 "authenticate" the client by setting the auth.localStorageKey in localStorage to some string value (can be anything for now)
 
 // 🐨 create a user using `buildUser`
